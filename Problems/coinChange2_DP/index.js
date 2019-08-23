435788def11a3572a97d0d10536a1e9fa6ea9bd4
Nitin <nitin.j@quantinsti.com>
2019-08-23 13:17:06 +0530

    coin-change

---
+++ Problems/coinChange2_DP/index.js
@@ -0,0 +1,41 @@
+const coins = [1, 2, 3, 5, 10, 3, 6, 8, 9, 22];
+const total = 150;
+const result = coinChange(coins, total);
+console.log(result.sort((a, b) => a > b))
+
+function coinChange(coins, total) {
+    const resultArray = [];
+    for (let i = 0; i < coins.length; i++) {
+        resultArray[i] = new Array();
+        for (let j = 0; j <= total; j++) {
+            if (j == 0) {
+                resultArray[i][j] = 0;
+                continue;
+            }
+            if (coins[i] > j) {
+                resultArray[i][j] = i > 0 ? resultArray[i - 1][j] : 0;
+            } else {
+                resultArray[i][j] = Math.min(
+                    (i > 0 ? resultArray[i - 1][j] : Infinity),
+                    1 + resultArray[i][j - coins[i]]
+                );
+            }
+        }
+    }
+    let totalRemaining = total;
+    let i = coins.length - 1;
+    let j = total;
+    const denominationPicked = [];
+    while (totalRemaining !== 0) {
+        if (resultArray[i][j] === resultArray[i - 1][j]) {
+            i--;
+            continue;
+        }
+        const pickCoin = coins[i];
+        denominationPicked.push(pickCoin);
+        totalRemaining = j - pickCoin;
+        j = totalRemaining;
+    }
+    console.log('Denominations Picked for Total ', total, ': ', denominationPicked)
+    return denominationPicked;
+}
---
+++ Problems/coinChange2_DP/readme.md
@@ -0,0 +1,9 @@
+## Coin Change - Dynamic Programming
+Minimum denominations to get total from available denominations.<br/>
+```
+Coins: { 2, 3, 5, 10 }
+Total: 15
+
+Denominations Picked: {5, 10}
+```
+https://www.youtube.com/watch?v=J2eoCvk59Rc&list=PLdo5W4Nhv31aBrJE1WS4MR9LRfbmZrAQu&index=3

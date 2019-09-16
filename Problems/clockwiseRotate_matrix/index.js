8044bb3e308a28258d0d35d842693a82f4a264a8
Nitin <nitin.j@quantinsti.com>
2019-09-16 14:55:26 +0530

    clockwise

---
+++ Problems/clockwiseRotate_matrix/index.js
@@ -0,0 +1,20 @@
+const rotate = (arr, transpose = []) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (!transpose[j]) {
+                transpose[j] = [];
+            }
+            transpose[j][i] = arr[i][j];
+        }
+    }
+    transpose.map(el => el.reverse())
+    console.log(transpose)
+}
+
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+rotate(arr);

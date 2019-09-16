9df70bdb885cf8f71539dd4c960ce6cfc7b8d2dd
Nitin <nitin.j@quantinsti.com>
2019-09-16 15:57:01 +0530

    matrix

--- Problems/transpose_matrix/index.js
+++ Problems/transpose_matrix/index.js
@@ -7,7 +7,23 @@ const transpose = (arr, transpose = []) => {
             transpose[j][i] = arr[i][j];
         }
     }
-    console.log(transpose)
+    console.log('Transpose: ', transpose)
+}
+
+const transposeInPlace = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < i; j++) {
+            arr[i][j] = arr[i][j] + arr[j][i];
+            arr[j][i] = arr[i][j] - arr[j][i];
+            arr[i][j] = arr[i][j] - arr[j][i];
+        }
+    }
+    console.log('Transpose In Place: ', arr)
+}
+
+const transposeAdvance = (arr) => {
+    const transpose = arr.map((col, i) => arr.map(row => row[i]));
+    console.log('TransposeAdvance :', transpose)
 }
 
 const arr = [
@@ -16,4 +32,18 @@ const arr = [
     [7, 8, 9]
 ];
 
+const arrForInPlace = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+const arrForAdv = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
 transpose(arr);
+transposeInPlace(arrForInPlace);
+transposeAdvance(arrForAdv);

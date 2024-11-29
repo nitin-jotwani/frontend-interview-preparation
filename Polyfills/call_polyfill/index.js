3fd722036ebdf19236c9a029652f3d4a0735420f
Nitin Jotwani <jotwani.nikki@gmail.com>
2024-11-29 21:50:10 +0530

    Update index.js

--- Polyfills/call_polyfill/index.js
+++ Polyfills/call_polyfill/index.js
@@ -10,6 +10,11 @@ Function.prototype.myCall = function(someOtherThis) {
     eval("someOtherThis.fnName(" + args + ")");
 }
 
+Function.prototype.myCall_v2 = function myCall(thisArg, ...params) {
+    thisArg.functionToCall = this
+    thisArg.functionToCall(...params)
+}
+
 function showProfileMessage(arg1, arg2) {
     console.log('showProfileMessage:', this.name, arg1, arg2);
 }

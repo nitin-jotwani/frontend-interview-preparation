f85efe6ca613c2a9230932778293dc62770d3c46
Nitin Jotwani <jotwani.nikki@gmail.com>
2024-11-29 21:59:51 +0530

    Update index.js

--- Polyfills/apply_polyfill/index.js
+++ Polyfills/apply_polyfill/index.js
@@ -1,21 +1,4 @@
-Function.prototype.myCall = function(someOtherThis) {
-    someOtherThis.fnName = this;
-    // someOtherThis.fnName(); // This will work, but without args
-
-    // to handle args we will use eval
-    const args = arguments[1];
-    const newArray = [];
-    args.forEach((ele, index) => {
-        newArray.push('args[' + index +']');
-    })
-    eval("someOtherThis.fnName(" + newArray + ")");
+Function.prototype.myApply = function app(thisContext, params) {
+    thisContext.fn = this
+    thisContext.fn(...params)
 }
-
-function showProfileMessage(arg1, arg2) {
-    console.log('showProfileMessage:', this.name, arg1, arg2);
-}
-
-const obj = {
-    name: "Nitin"
-}
-showProfileMessage.myCall(obj, ['Dude', 'Yoo']);

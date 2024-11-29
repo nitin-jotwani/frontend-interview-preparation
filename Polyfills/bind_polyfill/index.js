3d1e20e883560c7ff452e0263f9fabd99dfa2960
Nitin Jotwani <jotwani.nikki@gmail.com>
2024-11-29 22:15:00 +0530

    Update index.js

--- Polyfills/bind_polyfill/index.js
+++ Polyfills/bind_polyfill/index.js
@@ -16,3 +16,14 @@ const obj = {
 
 const boundFunc = methodToRun.myBind(obj, 'Dude', 'Yoo');
 boundFunc();
+
+// Using spread and Rest
+Function.prototype.myBind = function fn(thisContext, ...params) {
+    thisContext.fn = this
+    return () => {
+        thisContext.fn(...params)
+    }
+}
+
+const boundFunc = obj.func1.myBind({f1: 'pqr'}, 'b1param1', 'b1param2')
+boundFunc()
